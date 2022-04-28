d2b34d46595d40182d158e813799e5866b5c2ff0
YuDemura <Yu.demura1214@gmail.com>
2022-04-28 21:33:17 +0900

    ログイン画面リファクタリング

--- src/public/user/signin.php
+++ src/public/user/signin.php
@@ -1,81 +1,41 @@
 <?php
 session_start();
-$msg = [];
-if ($_POST) {
-    $email = filter_input(INPUT_POST, 'email');
-    $password = filter_input(INPUT_POST, 'password');
+$registed = $_SESSION['registed'] ?? '';
+$error = $_SESSION['errors'] ?? '';
 
-    if (!$email) {
-        $msg[] = 'メールアドレスを入力してください';
-    }
-
-    if (!$password) {
-        $msg[] = 'パスワードを入力してください';
-    }
-
-    if (!$msg) {
-        $dbUserName = 'root';
-        $dbPassword = 'password';
-        try {
-            $pdo = new PDO('mysql:host=mysql; dbname=blog; charset=utf8', $dbUserName, $dbPassword);
-        } catch (PDOException $e) {
-            $msg[] = $e->getMessage();
-        }
-
-        $sql = "SELECT * FROM users WHERE email = :email";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-        $stmt->execute();
-        $member = $stmt->fetch();
-        if ($member && $member['email'] === $email) {
-            if (password_verify(trim($_POST['password']), $member['password'])) {
-            session_regenerate_id(true);
-            $_SESSION['email'] = $member['email'];
-            $_SESSION['password'] = $member['password'];
-            $_SESSION['user_id'] = $member['id'];
-            $_SESSION['name'] = $member['name'];
-            header('Location: /index.php');
-            } else {
-                $msg[] = 'パスワードが違います。';
-            }
-        } else {
-            $msg[] = 'メールアドレスが違います。';
-        }
-    }
-}
+$_SESSION['registed'] = '';
+unset($_SESSION['errors']);
 ?>
 
-<form action="/user/signin.php" method="post">
-    <body>
-    <?php if ($msg) : ?>
-        <?php foreach ($msg as $m) : ?>
-            <p><?php echo $m ?></p>
-        <?php endforeach; ?>
-        <?php endif; ?>
-    <table>
-        <tr>
-            <td><p>ログイン</p></td>
-        </tr>
-        <tr>
-        </tr>
-        <tr>
-            <td><input type="email" name="email" placeholder="Email"></td>
-        </tr>
-        <tr>
-        </tr>
-        <tr>
-            <td><input type="password" name="password" placeholder="Password"></td>
-        </tr>
-        <tr>
-        </tr>
-        <tr>
-            <td><input type="submit" value="ログイン"></td>
-        </tr>
-        <tr>
-        </tr>
-        <tr>
-            <td><a href="./signup.php">アカウントを作る</a></td>
-        </tr>
-    </table>
-    </body>
-</form>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gray-200 w-full h-screen flex justify-center items-center">
+    <div class="w-96  bg-white pt-10 pb-10 rounded-xl">
+        <div class="w-60 m-auto text-center">
+            <h2 class="text-2xl mb-5">ログイン</h2>
+            <h3 class="mb-5 text-xl"><?php echo $registed; ?></h3>
+            <p class="text-red-600"><?php echo $error; ?></p>
+            <form class="px-4" action="./signin_complete.php" method="POST">
+                <p><input class="border-2 border-gray-300 mb-5 w-full" type=“text” name="email" type="mail" required placeholder="Email" value="<?php if (
+                    isset($_SESSION['email'])
+                ) {
+                    echo $_SESSION['email'];
+                } ?>"></p>
+                <p><input class="border-2 border-gray-300 mb-5 w-full" type="password" placeholder="Password" name="password"></p>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mb-5 w-full" type="submit">ログイン</button>
+            </form>
+            <a class="text-blue-500" href="./signup.php">アカウントを作る</a>
+        </div>
+    </div>
+</body>
+
+</html>
