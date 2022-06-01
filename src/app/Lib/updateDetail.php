345cc63e4f43a9ee5f1625ae0ed33493a4080061
YuDemura <Yu.demura1214@gmail.com>
2022-06-01 16:47:57 +0900

    記事編集処理関数化

---
+++ src/app/Lib/updateDetail.php
@@ -0,0 +1,15 @@
+<?php
+require_once(__DIR__ . '/../Lib/pdoInit.php');
+
+function updateDetail(string $blog_id, string $user_id, string $title, string $contents): void
+{
+	$pdo = pdoInit();
+
+	$sql = 'UPDATE blogs SET title=:title, contents=:contents WHERE id = :id and user_id=:user_id';
+	$statement = $pdo->prepare($sql);
+	$statement->bindValue(':title', $title, PDO::PARAM_STR);
+	$statement->bindValue(':contents', $contents, PDO::PARAM_STR);
+	$statement->bindParam(':id', $blog_id, PDO::PARAM_INT);
+	$statement->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+	$statement->execute();
+}
--- src/public/update.php
+++ src/public/update.php
@@ -1,27 +1,13 @@
 <?php
+require_once(__DIR__ . '/../app/Lib/updateDetail.php');
+require_once(__DIR__ . '/../app/Lib/redirect.php');
 session_start();
-
-$dbUserName = 'root';
-$dbPassword = 'password';
-try {
-    $pdo = new PDO('mysql:host=mysql; dbname=blog; charset=utf8', $dbUserName, $dbPassword);
-} catch (PDOException $e) {
-    $msg = $e->getMessage();
-}
 
 $blog_id = filter_input(INPUT_POST, 'id');
 $title = filter_input(INPUT_POST, 'title');
 $contents = filter_input(INPUT_POST, 'contents');
 $user_id = $_SESSION['user_id'];
 
-$sql = 'UPDATE blogs SET title=:title, contents=:contents WHERE id = :id and user_id=:user_id';
-$statement = $pdo->prepare($sql);
-$statement->bindValue(':id', $blog_id, PDO::PARAM_INT);
-$statement->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-$statement->bindValue(':title', $title, PDO::PARAM_STR);
-$statement->bindValue(':contents', $contents, PDO::PARAM_STR);
-$statement->execute();
-
-header("Location: /myarticledetail.php?id=$blog_id");
-exit();
+updateDetail($blog_id, $user_id, $title, $contents);
+redirect("myarticledetail.php?id=$blog_id");
 ?>
