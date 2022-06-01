05f95bbf7eecad21119c0462597d09b08538cf14
YuDemura <Yu.demura1214@gmail.com>
2022-06-01 17:20:59 +0900

    コメント投稿関数EOF追加

--- src/app/Lib/commentToPost.php
+++ src/app/Lib/commentToPost.php
@@ -5,7 +5,15 @@ function commentToPost(string $blog_id, string $user_id): ?array
 {
 	$pdo = pdoInit();
 
-	$sql = "SELECT commenter_name, comments, created_at FROM comments WHERE blog_id=$blog_id and user_id=$user_id";
+	$sql = <<<EOF
+		SELECT
+			commenter_name, comments, created_at
+		FROM
+			comments
+		WHERE
+			blog_id=$blog_id and user_id=$user_id
+		;
+	EOF;
 	$statement = $pdo->prepare($sql);
 	$statement->execute();
 	$comments_post = $statement->fetchAll(PDO::FETCH_ASSOC);
