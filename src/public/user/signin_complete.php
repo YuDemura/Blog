e9532158a2e7e276f43f3f87e5257a0f74b36213
YuDemura <Yu.demura1214@gmail.com>
2022-06-09 15:29:52 +0900

    ログイン処理リファクタリング

--- src/public/user/signin_complete.php
+++ src/public/user/signin_complete.php
@@ -1,19 +1,18 @@
 <?php
-require_once(__DIR__ . '/../../app/Lib/session.php');
 require_once(__DIR__ . '/../../app/Lib/redirect.php');
 require_once(__DIR__ . '/../../app/Lib/login.php');
-
+require_once __DIR__ . '/../../vendor/autoload.php';
+use App\Lib\Session;
 
 $email = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'password');
 
 $session = Session::getInstance();
-// var_dump($session);die;
 $formInputs = [
     'email' => $email
 ];
 $session->setFormInputs($formInputs);
-// var_dump($session);die;
+
 if (empty($email) || empty($password)) {
     $session->appendError("パスワードとメールアドレスを入力してください");
     redirect('signin.php');
@@ -27,15 +26,11 @@ if (!password_verify($password, $member['password'])) {
     $session->appendError("メールアドレスまたはパスワードが違います");
     redirect('signin.php');
 }
-// var_dump($member['id']);die;
-// var_dump($member['name']);die;
+
 $formInputs = [
     'user_id' => $member['id'],
     'name' => $member['name']
 ];
-// var_dump($formInputs);die;
 $session->setFormInputs($formInputs);
-
-// var_dump($_SESSION['user_id']);die;
 
 redirect('../index.php');
