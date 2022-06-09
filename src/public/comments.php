01984df466be98195285a1a7b448492e6f3c7141
YuDemura <Yu.demura1214@gmail.com>
2022-06-09 15:35:46 +0900

    コメント投稿処理リファクタリング

--- src/public/comments.php
+++ src/public/comments.php
@@ -1,12 +1,11 @@
 <?php
 require_once(__DIR__ . '/../app/Lib/postComment.php');
 require_once(__DIR__ . '/../app/Lib/redirect.php');
-require_once(__DIR__ . '/../app/Lib/session.php');
+require_once __DIR__ . '/../vendor/autoload.php';
+use App\Lib\Session;
 $session = Session::getInstance();
-$formInputs = [
-    'user_id' => $user_id
-];
-$session->setFormInputs($formInputs);
+$formInputs = $session->getFormInputs();
+$user_id = $formInputs['user_id'];
 $blog_id = $_POST['id'];
 $commenter_name = filter_input(INPUT_POST, 'commenter_name');
 $comments = filter_input(INPUT_POST, 'comments');
