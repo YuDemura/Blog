10e95464286bf1a805f4e3223b21cdbb2dea1e58
YuDemura <Yu.demura1214@gmail.com>
2022-07-04 21:51:56 +0900

    UseCaseInput作成

---
+++ src/app/Usecase/UseCaseInput/SignUpInput.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Usecase\UseCaseInput;
+
+final class SignUpInput
+{
+    private $name;
+    private $email;
+    private $password;
+
+    public function __construct(string $name, string $email, string $password)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function email(): string
+    {
+        return $this->email;
+    }
+
+    public function password(): string
+    {
+        return $this->password;
+    }
+}
