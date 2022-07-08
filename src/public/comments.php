27fca61d245500ccad5cfb1881394a8a81cdf7da
YuDemura <Yu.demura1214@gmail.com>
2022-07-08 16:25:27 +0900

    コメント投稿処理ページへUseCaseを追記

--- src/public/comments.php
+++ src/public/comments.php
@@ -1,9 +1,9 @@
 <?php
-require_once(__DIR__ . '/../app/Infrastructure/Dao/CommentDao.php');
 require_once __DIR__ . '/../app/Infrastructure/Redirect/redirect.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 use App\Lib\Session;
-use App\Infrastructure\Dao\CommentDao;
+use App\Usecase\CommentInput\CommentInput;
+use App\Usecase\CommentInteractor\CommentInteractor;
 
 $session = Session::getInstance();
 $formInputs = $session->getFormInputs();
@@ -12,7 +12,12 @@ $blog_id = $_POST['id'];
 $commenter_name = filter_input(INPUT_POST, 'commenter_name');
 $comments = filter_input(INPUT_POST, 'comments');
 
-$commentDao = new CommentDao();
-$commentDao->postComment($user_id, $blog_id, $commenter_name, $comments);
-redirect("/detail.php?id=$blog_id");
+$useCaseInput = new CommentInput($user_id, $blog_id, $commenter_name, $comments);
+$useCase = new CommentInteractor($useCaseInput);
+$useCaseOutput = $useCase->post();
+
+if ($useCaseOutput->issuccess()) {
+    redirect("/detail.php?id=$blog_id");
+}
+
 ?>
