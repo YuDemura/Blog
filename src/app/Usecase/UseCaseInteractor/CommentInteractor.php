e31e5a27c27b0be006a74af9cf786a7bc0c8ecd1
YuDemura <Yu.demura1214@gmail.com>
2022-07-09 05:13:13 +0900

    CommentInteractorクラスへエラーが起きる場合の処理追加

--- src/app/Usecase/UseCaseInteractor/CommentInteractor.php
+++ src/app/Usecase/UseCaseInteractor/CommentInteractor.php
@@ -16,18 +16,24 @@ final class CommentInteractor
         $this->input = $input;
     }
 
-    public function post(): CommentOutput
+    public function run(): CommentOutput
     {
-        $session = Session::getInstance();
-        $formInputs = $session->getFormInputs();
-        $user_id = $formInputs['user_id'];
-
-        $blog_id = $this->input->blog_id();
-        $commenter_name = $this->input->commenter_name();
-        $comments = $this->input->comments();
-
-        $commentDao = new CommentDao();
-        $commentDao->postComment($user_id, $blog_id, $commenter_name, $comments);
-        return new CommentOutput(true);
+        try {
+            $session = Session::getInstance();
+            $formInputs = $session->getFormInputs();
+            $user_id = $formInputs['user_id'];
+            if (!$user_id) {
+                throw new Exception("ユーザーIDが一致しません");
+            }
+            $blog_id = $this->input->blog_id();
+            $commenter_name = $this->input->commenter_name();
+            $comments = $this->input->comments();
+            $commentDao = new CommentDao();
+            $commentDao->postComment($user_id, $blog_id, $commenter_name, $comments);
+            return new CommentOutput(true);
+        } catch (Exception $e) {
+            $e->getMessage();
+            return new CommentOutput(false);
+        }
     }
 }
