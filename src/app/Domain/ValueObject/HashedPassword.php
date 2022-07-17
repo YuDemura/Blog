945b2039a46556cc5ed6b5b8ba751a7e06dfec21
YuDemura <Yu.demura1214@gmail.com>
2022-07-17 12:58:55 +0900

    ValueObject HashedPasswordクラス作成

---
+++ src/app/Domain/ValueObject/HashedPassword.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Domain\ValueObject;
+use Exception;
+
+/**
+ * ハッシュ化したパスワード用のValueObject
+ */
+final class HashedPassword
+{
+    /**
+     * パスワードが不正な場合のエラーメッセージ
+     */
+    const INVALID_MESSAGE = 'パスワードの形式が正しくありません';
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * コンストラクタ
+     *
+     * @param string $value
+     */
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * パスワードの照合
+     *
+     * @param InputPassword $inputPassword
+     * @return bool
+     */
+    public function verify(InputPassword $inputPassword): bool
+    {
+        return !password_verify($inputPassword->value(), $this->value);
+    }
+}
