dcd53cd8c50af70ebce7d4874176f2d6d7593298
YuDemura <Yu.demura1214@gmail.com>
2022-07-21 00:15:29 +0900

    DeleteInputへValueObjectクラス追記

--- src/app/Usecase/UseCaseInput/DeleteInput.php
+++ src/app/Usecase/UseCaseInput/DeleteInput.php
@@ -1,16 +1,19 @@
 <?php
 namespace App\Usecase\UseCaseInput;
 
+use App\Domain\ValueObject\BlogId;
+
+
 final class DeleteInput
 {
     private $blog_id;
 
-    public function __construct(string $blog_id)
+    public function __construct(BlogId $blog_id)
     {
         $this->blog_id = $blog_id;
     }
 
-    public function blog_id(): string
+    public function blog_id(): BlogId
     {
         return $this->blog_id;
     }
