48e319990bbcfd033d2209f680a713edbed069b3
YuDemura <Yu.demura1214@gmail.com>
2022-07-20 23:55:46 +0900

    記事編集処理へValueObject追記

--- src/public/update.php
+++ src/public/update.php
@@ -5,6 +5,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\Lib\Session;
 use App\Usecase\UseCaseInput\EditInput;
 use App\Usecase\UseCaseInteractor\EditInteractor;
+use App\Domain\ValueObject\UserId;
+use App\Domain\ValueObject\Title;
+use App\Domain\ValueObject\Contents;
+use App\Domain\ValueObject\BlogId;
 
 $session = Session::getInstance();
 $formInputs = $session->getFormInputs();
@@ -13,7 +17,11 @@ $blog_id = filter_input(INPUT_POST, 'id');
 $title = filter_input(INPUT_POST, 'title');
 $contents = filter_input(INPUT_POST, 'contents');
 
-$useCaseInput = new EditInput($blog_id, $user_id, $title, $contents);
+$BlogId = new BlogId($blog_id);
+$UserId = new UserId($user_id);
+$Title = new Title($title);
+$Contents = new Contents($contents);
+$useCaseInput = new EditInput($BlogId, $UserId, $Title, $Contents);
 $useCase = new EditInteractor($useCaseInput);
 $useCaseOutput = $useCase->run();
 if ($useCaseOutput->isSuccess()) {
