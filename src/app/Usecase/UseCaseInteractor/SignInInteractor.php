65219572d04b435d6aff0936981d23403143b902
YuDemura <Yu.demura1214@gmail.com>
2022-07-28 12:22:04 +0900

    SignInInteractorへEntityクラスの処理追記

--- src/app/Usecase/UseCaseInteractor/SignInInteractor.php
+++ src/app/Usecase/UseCaseInteractor/SignInInteractor.php
@@ -4,6 +4,11 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 use App\Lib\Session;
 use App\Lib\SessionKey;
 require_once __DIR__ . '/../../Infrastructure/Redirect/redirect.php';
+use App\Domain\Entity\User;
+use App\Domain\ValueObject\UserId;
+use App\Domain\ValueObject\UserName;
+use App\Domain\ValueObject\Email;
+use App\Domain\ValueObject\HashedPassword;
 use App\Usecase\UseCaseInput\SignInInput;
 use App\Usecase\UseCaseOutput\SignInOutput;
 use App\Infrastructure\Dao\UserDao;
@@ -24,13 +29,15 @@ final class SignInInteractor
 
     public function handler(): SignInOutput
     {
-        $user = $this->findUser();
+        $userMapper = $this->findUser();
 
-        if ($this->notExistsUser($user)) {
+        if ($this->notExistsUser($userMapper)) {
             return new SignInOutput(false, self::FAILED_MESSAGE);
         }
 
-        if ($this->isInvalidPassword($user['password'])) {
+        $user = $this->buildUserEntity($userMapper);
+
+        if ($this->isInvalidPassword($user->password()->value())) {
             return new SignInOutput(false, self::FAILED_MESSAGE);
         }
 
@@ -48,17 +55,26 @@ final class SignInInteractor
         return is_null($user);
     }
 
+    private function buildUserEntity(array $user): User
+    {
+        return new User(
+            new UserId($user['id']),
+            new UserName($user['name']),
+            new Email($user['email']),
+            new HashedPassword($user['password']));
+    }
+
     private function isInvalidPassword(?string $password): bool
     {
         return !password_verify($this->input->password()->value(), $password);
     }
 
-    private function saveSession(array $user): void
+    private function saveSession(User $user): void
     {
         $session = Session::getInstance();
         $formInputs = [
-        'user_id' => $user['id'],
-        'name' => $user['name']
+        'user_id' => $user->id(),
+        'name' => $user->name()
         ];
         $session->setFormInputs($formInputs);
     }
