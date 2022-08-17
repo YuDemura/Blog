91e68f6e2568c59515488bd70dcbfd1fb02d5939
YuDemura <Yu.demura1214@gmail.com>
2022-08-17 13:12:01 -0400

    SignUpInteractorへQueryServiceとRepository追記

--- src/app/Usecase/UseCaseInteractor/SignUpInteractor.php
+++ src/app/Usecase/UseCaseInteractor/SignUpInteractor.php
@@ -4,33 +4,56 @@ namespace App\Usecase\UseCaseInteractor;
 require_once __DIR__ . '/../../../vendor/autoload.php';
 use App\Usecase\UseCaseInput\SignUpInput;
 use App\Usecase\UseCaseOutput\SignUpOutput;
-use App\Infrastructure\Dao\UserDao;
+use App\Adapter\QueryService\UserQueryService;
+use App\Adapter\Repository\UserRepository;
+use App\Domain\ValueObject\NewUser;
+use App\Domain\Entity\User;
 
 final class SignUpInteractor
 {
     const ALLREADY_EXISTS_MESSAGE = 'すでに登録済みのメールアドレスです';
     const COMPLETED_MESSAGE = '登録が完了しました';
 
-    private $useCaseInput;
+    private $userRepository;
+    private $userQueryService;
+    private $input;
 
-    public function __construct(SignUpInput $useCaseInput)
+    public function __construct(SignUpInput $input)
     {
-        $this->useCaseInput = $useCaseInput;
+        $this->userRepository = new UserRepository();
+        $this->userQueryService = new UserQueryService();
+        $this->input = $input;
     }
 
     public function handler(): SignUpOutput
     {
-        $userDao = new UserDao();
-        $user = $userDao->findUserByMail($this->useCaseInput->email()->value());
-        if ($user) {
+        $user = $this->findUser();
+        if ($this->existsUser($user)) {
             return new SignUpOutput(false, self::ALLREADY_EXISTS_MESSAGE);
         }
 
-        $userDao->createUser(
-            $this->useCaseInput->name()->value(),
-            $this->useCaseInput->email()->value(),
-            $this->useCaseInput->password()->value()
-        );
+        $this->signup();
         return new SignUpOutput(true, self::COMPLETED_MESSAGE);
+    }
+
+    private function findUser(): ?User
+    {
+        return $this->userQueryService->findUserByMail($this->input->email());
+    }
+
+    private function existsUser(?User $user): bool
+    {
+        return !is_null($user);
+    }
+
+    private function signup(): void
+    {
+        $this->userRepository->insert(
+            new NewUser(
+                $this->input->name(),
+                $this->input->email(),
+                $this->input->password()
+            )
+        );
     }
 }
