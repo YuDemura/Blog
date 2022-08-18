39038b1884de45a409a475c6a5f7118c2df45340
YuDemura <Yu.demura1214@gmail.com>
2022-08-18 01:19:24 -0400

    CreateBlogInteractor用のBlogRepository作成

---
+++ src/app/Adapter/Repository/BlogRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Adapter\Repository;
+require_once __DIR__ . '/../../../vendor/autoload.php';
+use App\Infrastructure\Dao\BlogDao;
+use App\Domain\ValueObject\NewBlog;
+
+final class BlogRepository
+{
+    /**
+     * @var BlogDao
+     */
+    private $blogDao;
+
+    public function __construct()
+    {
+        $this->blogDao = new BlogDao();
+    }
+
+    public function insert(NewBlog $blog): void
+    {
+        $this->blogDao->create($blog);
+    }
+
+
+
+
+
+
+
+
+
+}
