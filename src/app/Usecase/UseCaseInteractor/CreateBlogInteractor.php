5f0b42baaa161fa5ae371b13ad5ae3bea951ab6f
YuDemura <Yu.demura1214@gmail.com>
2022-08-18 01:21:17 -0400

    CreateBlogInteractorへRepository追記

--- src/app/Usecase/UseCaseInteractor/CreateBlogInteractor.php
+++ src/app/Usecase/UseCaseInteractor/CreateBlogInteractor.php
@@ -5,28 +5,42 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 use App\Lib\Session;
 use App\Usecase\UseCaseInput\CreateBlogInput;
 use App\Usecase\UseCaseOutput\CreateBlogOutput;
-use App\Infrastructure\Dao\BlogDao;
+use App\Adapter\Repository\BlogRepository;
+use App\Domain\ValueObject\NewBlog;
+
 
 final class CreateBlogInteractor
 {
+    /**
+     * @var BlogRepository
+     */
+    private $blogRepository;
+
+    /**
+     * @var CreateBlogInput
+     */
     private $input;
 
     public function __construct(CreateBlogInput $input)
     {
+        $this->blogRepository = new BlogRepository();
         $this->input = $input;
     }
 
     public function createBlog(): CreateBlogOutput
     {
-      $session = Session::getInstance();
-      $formInputs = $session->getFormInputs();
-      $user_id = $formInputs['user_id'];
-
-      $title = $this->input->title()->value();
-      $contents = $this->input->contents()->value();
-
-      $blogDao = new BlogDao();
-      $blogDao->create($user_id->value(), $title, $contents);
+      $this->blogup();
       return new CreateBlogOutput(true);
+    }
+
+    private function blogup(): void
+    {
+        $this->blogRepository->insert(
+            new NewBlog(
+                $this->input->user_id(),
+                $this->input->title(),
+                $this->input->contents()
+            )
+        );
     }
 }
