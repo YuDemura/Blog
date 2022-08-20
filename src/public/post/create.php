647face2c949559ca2ea6a58206dcc0ae7524cc7
YuDemura <Yu.demura1214@gmail.com>
2022-08-20 12:33:48 -0400

    create.phpから登録処理部分を削除

--- src/public/post/create.php
+++ src/public/post/create.php
@@ -1,33 +1,3 @@
-<?php
-require_once __DIR__ . '/../../app/Infrastructure/Redirect/redirect.php';
-require_once __DIR__ . '/../../vendor/autoload.php';
-use App\Lib\Session;
-use App\Usecase\UseCaseInput\CreateBlogInput;
-use App\Usecase\UseCaseInteractor\CreateBlogInteractor;
-use App\Domain\ValueObject\UserId;
-use App\Domain\ValueObject\Title;
-use App\Domain\ValueObject\Contents;
-
-$session = Session::getInstance();
-if ($session){
-    if ($_POST) {
-        $formInputs = $session->getFormInputs();
-        $user_id = $formInputs['user_id'];
-        $title = filter_input(INPUT_POST, 'title');
-        $contents = filter_input(INPUT_POST, 'contents');
-        $userId = new UserId($user_id->value());
-        $Title = new Title($title);
-        $Contents = new Contents($contents);
-        $useCaseInput = new CreateBlogInput($userId, $Title, $Contents);
-        $useCase = new CreateBlogInteractor($useCaseInput);
-        $useCaseOutput = $useCase->createBlog();
-        if ($useCaseOutput->isSuccess()) {
-            redirect('/../../mypage.php');
-        }
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -35,7 +5,7 @@ if ($session){
     <title>新規記事</title>
 </head>
 <body>
-    <form action="" method="POST">
+    <form action="./store.php" method="POST">
         <table>
             <tr>
                 <td><p>新規記事</p></td>
