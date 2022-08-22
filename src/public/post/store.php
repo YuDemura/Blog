d7ac00a69e26ca998f628ac6cb17e6e64c1050ac
YuDemura <Yu.demura1214@gmail.com>
2022-08-22 12:58:06 -0400

    store.phpをガード節へリファクタリングし、エラーメッセージ保存追記。

--- src/public/post/store.php
+++ src/public/post/store.php
@@ -9,27 +9,32 @@ use App\Domain\ValueObject\Title;
 use App\Domain\ValueObject\Contents;
 
 $session = Session::getInstance();
-if ($session){
-    if ($_POST) {
-        $formInputs = $session->getFormInputs();
-        $user_id = $formInputs['user_id'];
-        $title = filter_input(INPUT_POST, 'title');
-        $contents = filter_input(INPUT_POST, 'contents');
-        $userId = new UserId($user_id->value());
-        $Title = new Title($title);
-        $Contents = new Contents($contents);
-        $useCaseInput = new CreateBlogInput($userId, $Title, $Contents);
-        $useCase = new CreateBlogInteractor($useCaseInput);
-        $useCaseOutput = $useCase->handler();
-        if ($useCaseOutput->isSuccess()) {
-            redirect('/../../mypage.php');
-        } else {
-            redirect('./create.php');
-        }
-    } else {
-        redirect('./create.php');
-    }
-} else {
+if (!$session){
     redirect('./user/signin.php');
 }
+
+if (!$_POST) {
+    redirect('/../../index.php');
+}
+
+if ($_POST) {
+    $formInputs = $session->getFormInputs();
+    $user_id = $formInputs['user_id'];
+    $title = filter_input(INPUT_POST, 'title');
+    $contents = filter_input(INPUT_POST, 'contents');
+    $userId = new UserId($user_id->value());
+    $Title = new Title($title);
+    $Contents = new Contents($contents);
+    $useCaseInput = new CreateBlogInput($userId, $Title, $Contents);
+    $useCase = new CreateBlogInteractor($useCaseInput);
+    $useCaseOutput = $useCase->handler();
+    if ($useCaseOutput->isSuccess()) {
+        $session->setMessage($useCaseOutput->message());
+        redirect('/../../mypage.php');
+    } else {
+        $session->appendError($useCaseOutput->message());
+        redirect('./create.php');
+    }
+}
+
 ?>
