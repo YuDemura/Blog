b6fbdadcd3432367009008e486b4086e36ddeb20
YuDemura <Yu.demura1214@gmail.com>
2022-09-26 08:23:40 -0400

    UserQueryServiceインターフェースへ抽象メソッド追加

--- src/app/Domain/InterfaceMapper/UserQueryServiceInterface.php
+++ src/app/Domain/InterfaceMapper/UserQueryServiceInterface.php
@@ -2,9 +2,12 @@
 namespace App\Domain\InterfaceMapper;
 require_once __DIR__ . '/../../../vendor/autoload.php';
 use App\Domain\ValueObject\Email;
+use App\Domain\ValueObject\UserId;
 use App\Domain\Entity\User;
 
 interface UserQueryServiceInterface
 {
     public function findUserByMail(Email $email): ?User;
+
+    public function findUserById(UserId $id): ?User;
 }
