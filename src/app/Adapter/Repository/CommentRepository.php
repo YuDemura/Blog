a211a313747b6b2df3a13a8ca11d4bbd129d4f7a
YuDemura <Yu.demura1214@gmail.com>
2022-09-27 03:42:33 -0400

    CommentRepositoryへインターフェース追加

--- src/app/Adapter/Repository/CommentRepository.php
+++ src/app/Adapter/Repository/CommentRepository.php
@@ -4,8 +4,9 @@ namespace App\Adapter\Repository;
 require_once __DIR__ . '/../../../vendor/autoload.php';
 use App\Infrastructure\Dao\CommentDao;
 use App\Domain\ValueObject\NewComment;
+use App\Domain\InterfaceMapper\CommentRepositoryInterface;
 
-final class CommentRepository
+final class CommentRepository implements CommentRepositoryInterface
 {
     /**
      * @var CommentDao
