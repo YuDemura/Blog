c4b152aad6bf02f60d02b36e7b1d4685300362de
YuDemura <Yu.demura1214@gmail.com>
2022-10-03 07:44:36 -0400

    BlogQueryServiceインターフェース作成

--- src/app/Domain/InterfaceMapper/BlogQueryServiceInterface.php
+++ src/app/Domain/InterfaceMapper/BlogQueryServiceInterface.php
@@ -2,9 +2,12 @@
 namespace App\Domain\InterfaceMapper;
 require_once __DIR__ . '/../../../vendor/autoload.php';
 use App\Domain\ValueObject\BlogId;
+use App\Domain\ValueObject\UserId;
 use App\Domain\Entity\Blog;
 
 interface BlogQueryServiceInterface
 {
     public function findById(BlogId $blogId): ?Blog;
+
+    public function showDetail(UserId $userId, BlogId $blogId): ?Blog;
 }
