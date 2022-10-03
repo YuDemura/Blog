d75877e5721440ac7e3f28f9cd9617c1ecddbc79
YuDemura <Yu.demura1214@gmail.com>
2022-10-03 07:45:01 -0400

    BlogRepositoryインターフェース作成

--- src/app/Domain/InterfaceMapper/BlogRepositoryInterface.php
+++ src/app/Domain/InterfaceMapper/BlogRepositoryInterface.php
@@ -2,8 +2,11 @@
 namespace App\Domain\InterfaceMapper;
 require_once __DIR__ . '/../../../vendor/autoload.php';
 use App\Domain\ValueObject\NewBlog;
+use App\Domain\ValueObject\UpdateBlog;
 
 interface BlogRepositoryInterface
 {
     public function insert(NewBlog $blog): void;
+
+    public function updateBlog(UpdateBlog $blog): void;
 }
